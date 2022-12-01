2e6384026892cf9c1e0bedfe0da9df184915c330
Bilguun0118 <bi7guunt@gmail.com>
2022-12-01 10:45:06 +0800

    loop

---
+++ loop/while/index.js
@@ -0,0 +1,41 @@
+//while loop
+
+// let i = 0;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+//do while
+// let ii = 0;
+
+// do {
+//     console.log(ii);
+//     ii++;
+// } while (ii <= 5)
+
+//loop 1
+// let a = 0;
+// while (a <= 10) {
+//     console.log("utga =", a);
+//     a++;
+// }
+
+//loop 2
+// let b = 0;
+// while (b <= 10) {
+//     console.log("sondgoi = " + b);
+//     b++;
+// }
+
+
+//number digits
+let a = 12345678;
+
+while (a <= 0) {
+
+    let b = Math.round(a / 10)
+    console.log(b);
+    b++;
+
+}
